c9d6f562522bef725ee61be66a04827a2a5ce26c
Aleksey Lisun <insuline.ru@gmail.com>
2022-10-06 10:14:34 +0300

    ♻️ refactor: change data to my own

--- card.js
+++ card.js
@@ -7,11 +7,6 @@ const chalk = require("chalk");
 const inquirer = require("inquirer");
 const clear = require("clear");
 const open = require("open");
-const fs = require('fs');
-const request = require('request');
-const path = require('path');
-const ora = require('ora');
-const cliSpinners = require('cli-spinners');
 clear();
 
 const prompt = inquirer.createPromptModule();
@@ -25,32 +20,15 @@ const questions = [
             {
                 name: `Send me an ${chalk.green.bold("email")}?`,
                 value: () => {
-                    open("mailto:hi@anmolsingh.me");
+                    open("mailto:insuli.ne.ru@gmail.com");
                     console.log("\nDone, see you soon at inbox.\n");
                 }
             },
             {
-                name: `Download my ${chalk.magentaBright.bold("Resume")}?`,
+                name: `Subscribe to my telegram channel`,
                 value: () => {
-                    // cliSpinners.dots;
-                    const loader = ora({
-                        text: ' Downloading Resume',
-                        spinner: cliSpinners.material,
-                    }).start();
-                    let pipe = request('https://anmolsingh.me/api/resume').pipe(fs.createWriteStream('./anmol-resume.html'));
-                    pipe.on("finish", function () {
-                        let downloadPath = path.join(process.cwd(), 'anmol-resume.html')
-                        console.log(`\nResume Downloaded at ${downloadPath} \n`);
-                        open(downloadPath)
-                        loader.stop();
-                    });
-                }
-            },
-            {
-                name: `Schedule a ${chalk.redBright.bold("Meeting")}?`,
-                value: () => {
-                    open('https://calendly.com/anmol098/30min');
-                    console.log("\n See you at the meeting \n");
+                    open('https://t.me/insuline_lifestyle');
+                    console.log("\n See you at the telegram \n");
                 }
             },
             {
@@ -64,22 +42,22 @@ const questions = [
 ];
 
 const data = {
-    name: chalk.bold.green("             Anmol Pratap Singh"),
-    handle: chalk.white("@anmol098"),
-    work: `${chalk.white("Lead Software Engineer at")} ${chalk
+    name: chalk.bold.green("             Aleksey Lisun"),
+    handle: chalk.white("@insulineru"),
+    work: `${chalk.white("Web 3.0 Frontend Lead at")} ${chalk
         .hex("#2b82b2")
-        .bold("FootLoose Labs")}`,
-    twitter: chalk.gray("https://twitter.com/") + chalk.cyan("misteranmol"),
-    github: chalk.gray("https://github.com/") + chalk.green("anmol098"),
-    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("anmol098"),
-    web: chalk.cyan("https://anmolsingh.me"),
-    npx: chalk.red("npx") + " " + chalk.white("anmol"),
+        .bold("Via Protocol")}`,
+    twitter: chalk.gray("https://twitter.com/") + chalk.cyan("insulineru"),
+    github: chalk.gray("https://github.com/") + chalk.green("insulineru"),
+    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("insuline"),
+    telegram: chalk.cyan("https://t.me/insuline"),
+    npx: chalk.red("npx") + " " + chalk.white("insuline"),
 
     labelWork: chalk.white.bold("       Work:"),
     labelTwitter: chalk.white.bold("    Twitter:"),
     labelGitHub: chalk.white.bold("     GitHub:"),
     labelLinkedIn: chalk.white.bold("   LinkedIn:"),
-    labelWeb: chalk.white.bold("        Web:"),
+    labelTelegram: chalk.white.bold("   Telegram:"),
     labelCard: chalk.white.bold("       Card:")
 };
 
@@ -92,20 +70,10 @@ const me = boxen(
         `${data.labelTwitter}  ${data.twitter}`,
         `${data.labelGitHub}  ${data.github}`,
         `${data.labelLinkedIn}  ${data.linkedin}`,
-        `${data.labelWeb}  ${data.web}`,
+        `${data.labelTelegram}  ${data.telegram}`,
         ``,
         `${data.labelCard}  ${data.npx}`,
         ``,
-        `${chalk.italic(
-            "I am currently looking for new opportunities,"
-        )}`,
-        `${chalk.italic("my inbox is always open. Whether you have a")}`,
-        `${chalk.italic(
-            "question or just want to say hi, I will try "
-        )}`,
-        `${chalk.italic(
-            "my best to get back to you!"
-        )}`
     ].join("\n"),
     {
         margin: 1,
